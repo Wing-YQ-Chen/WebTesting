6dbda69cfe9ff36f7c25a2f04bbd2b2927d8d881
Wing Chen <13059381952@163.com>
2025-02-24 01:52:49 +0800

    ++

--- src/test/java/org/Testing/Web/Runner/TestRunner.java
+++ src/test/java/org/Testing/Web/Runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "org.Testing.Web.Steps",
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        plugin = {"pretty", "html:reports/cucumber-reports.html"},
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
